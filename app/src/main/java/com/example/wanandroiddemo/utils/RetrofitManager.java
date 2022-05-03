d4671b42e22dd5c4c045f82b9784d55d0efbae4d
LuoYu-Ying <3274627526@qq.com>
2022-05-03 13:21:33 +0800

    feature:添加了部分工具类

---
+++ app/src/main/java/com/example/wanandroiddemo/utils/RetrofitManager.java
@@ -0,0 +1,20 @@
+package com.example.wanandroiddemo.utils;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitManager {
+    private static final String baseUrl = "https://www.wanandroid.com/";
+    private RetrofitManager() {}
+
+    public static Retrofit getInstance() {
+        return RetrofitHolder.instance;
+    }
+
+    private static class RetrofitHolder {
+        private static final Retrofit instance = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+}
