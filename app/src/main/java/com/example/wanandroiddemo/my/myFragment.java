3aecd78c739e3cce663e6688e7e1d0ab2b431cf0
hdh25490 <13896922098@163.com>
2022-05-06 22:15:40 +0800

    feature:修改底部导航栏样式

--- app/src/main/java/com/example/wanandroiddemo/my/myFragment.java
+++ app/src/main/java/com/example/wanandroiddemo/my/myFragment.java
@@ -22,7 +22,7 @@ public class myFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_my, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
         return view;
     }
 }
